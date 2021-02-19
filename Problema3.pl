4bcbaf771a1732ccd363d48d71a0bbc9c5fb2d02
Gary <gortiz1490@gmail.com>
2021-02-19 06:02:12 -0600

    se agrego el insertar lista

--- Problema3.pl
+++ Problema3.pl
@@ -14,3 +14,8 @@ palindromo(Xs):-
 duplicar([],[]).
 duplicar([C|L1],[C,C|L2]):-duplicar(L1,L2).
 
+%insertar un elemento dado un indice
+insertar(X,L1,0,[X|L1]).
+insertar(X,[C|R],Pos,[C|R2]):-
+Pos1 is Pos-1,
+insertar(X,R,Pos1,R2), !.
