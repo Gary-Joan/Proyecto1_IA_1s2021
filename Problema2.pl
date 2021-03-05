d200e08e10f250cd0682280bcce96dd1f6f6be08
Gary <gortiz1490@gmail.com>
2021-03-05 16:23:18 -0600

    posible problema 2 terminado

--- Problema2.pl
+++ Problema2.pl
@@ -13,7 +13,45 @@ mujer(ines).
 mujer(ruth).
 mujer(soledad).
 
-esposo(lamar,ana_lucia).
-esposo(ana_lucia,lamar).
+pareja(lamar,ana_lucia).
+pareja(ana_lucia,lamar).
 
+pareja(ruth,feliciano).
+pareja(feliciano,ruth).
+
+pareja(diana,pedro).
+pareja(pedro,diana).
+
+pareja(jonas,soledad).
+pareja(soledad,jonas).
+
+padre(ana_lucia,ruth).
+padre(lamar,ruth).
+padre(ana_lucia,pedro).
+padre(lamar,pedro).
+
+padre(pedro,manuel).
+padre(diana,manuel).
+
+padre(ruth, ines).
+padre(feliciano,ines).
+padre(ruth,jonas).
+padre(feliciano,jonas).
+
+padre(jonas,daniel).
+padre(jonas,constancia).
+padre(soledad,daniel).
+padre(soledad,constancia).
 % Reglas
+
+imprimir_arbol(_) :- 
+write('                     Lamar - Ana lucia'),nl,
+write('              _______________|________________'),nl,
+write('             |                                |'),nl,
+write('            Ruth - Feliciano                  Pedro - Diana '),nl,
+write('               ___|___                              |'), nl,
+write('              |       |                           Manuel' ),nl,
+write('             Ines    Jonas - Soledad'),nl,
+write('                       ____|____ '),nl,
+write('                       |        | '),nl,
+write('                       Daniel Constancia').
