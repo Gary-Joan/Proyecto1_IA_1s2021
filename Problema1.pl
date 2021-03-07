bf2387c5639d127f1ea62c1993c83d529e904e31
sharolin <sharolin33@gmail.com>
2021-03-07 16:26:14 -0600

    revisando
    no hay cambios

--- Problema1.pl
+++ Problema1.pl
@@ -71,18 +71,26 @@ pareja(bruce,marta).
 
 padre(X,Y):-hombre(X),progenitor(X,Y).
 madre(X,Y):-mujer(X),progenitor(X,Y).
+
 hermanos(X,Y):-progenitor(Z,X),progenitor(Z,Y).
+
 hermano(X,Y):-hombre(X),hermanos(X,Y).
 hermana(X,Y):-mujer(X),hermanos(X,Y).
+
 esposo(X,Y):-pareja(X,Y),hombre(X).
 esposa(X,Y):-pareja(X,Y),mujer(X).
+
 suegro(X,Y):-padre(X,Z),pareja(Y,Z).
 suegra(X,Y):-madre(X,Z),pareja(Y,Z).
+
 yerno(X,Y):-suegro(Y,X);suegra(Y,X),hombre(X).
 nuera(X,Y):-suegro(Y,X);suegra(Y,X),mujer(X).
+
 abuelo(X,Y):-progenitor(Z,Y),padre(X,Z).
 abuela(X,Y):-progenitor(Z,Y),madre(X,Z).
+
 nieto(X,Y):-progenitor(Y,Z),progenitor(Z,X),hombre(X).
 nieta(X,Y):-progenitor(Y,Z),progenitor(Z,X),mujer(X).
+
 primo(X,Y):-progenitor(Z,X),progenitor(W,Y),hermanos(Z,W),hombre(X).
 prima(X,Y):-progenitor(Z,X),progenitor(W,Y),hermanos(Z,W),mujer(X).
